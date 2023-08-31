679a920a20be27fda14495ad689f31e725e8b641
Nader Zaman <naderzaman50@gmail.com>
2023-08-31 19:11:25 +0500

    Let and const

---
+++ 1.js
@@ -0,0 +1,11 @@
+// let and Const  // variables Constants 
+// Arrow Functions -- Functions
+// Template Literals  ''' and ` ` these back ticks are Template litrels
+// Default Parameters -- Jab aap Value na den function ko to wo default value le sake
+// Rest and Spread operators -- dono alag alag work krtay hn different context mn 
+// Destructuring arrays and object sy data bahar nikalnay k leay use hotay hn
+// Classes -- to make objects from a blue print
+// Modules
+// Promisses -- to take  care of async part
+// Async and Await -- to take care of async part but more 
+// Try Catch
---
+++ Let_and_const.js
@@ -0,0 +1,7 @@
+// let and Const
+
+let a = 3; // 
+ a = 4;    //  we can assign a new value if we are using let 
+
+ const b = 45;
+ b = 34;   //  we can not assign a new value to const if we are using const it will show an error on console
